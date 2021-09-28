3ccdb7a8b49a52f01f1b2d12ba6088903b7df8b3
mahmud009 <mahmud.est@gmail.com>
2021-09-28 21:24:41 +0600

    test api

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ app.get("/ping", (req, res) => {
   res.status(200).send("Ping success");
 });
 
+app.get("/products", (req, res) => {
+  const product = { name: "test product", id: 1 };
+  res.status(200).send(product);
+});
+
 app.listen(PORT, () => console.log(`App is running on ${PORT}`));
